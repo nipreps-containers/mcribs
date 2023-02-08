e810843dde3867c5c54c813a1a31b0ba397796ab
mathiasg <mathiasg@stanford.edu>
2023-02-08 13:54:01 -0500

    RF: Allow passing multiple directories

--- scripts/fixpy.sh
+++ scripts/fixpy.sh
@@ -2,10 +2,10 @@
 
 set -eu
 
-MIRTKDIR=$1
+DIRS=$@
 
-for TOOL in $(find $MIRTKDIR/lib/tools/ -exec file {} \; | grep text | cut -d: -f1) $MIRTKDIR/bin/mirtk; do
-    if [[ $(head -n1 $TOOL) == *"python" ]]; then
+for TOOL in $(find ${DIRS[@]} -exec file {} \; | grep text | cut -d: -f1); do
+    if [[ $(head -n1 $TOOL) == *"python"* ]]; then
         sed -i '1 c#! /usr/bin/env python' $TOOL && echo "Fixed: $TOOL";
     fi
 done
